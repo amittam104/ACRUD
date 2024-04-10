d04fbe9d5f869c25177260f64d039935893e7cf5
Amit Tambulkar <amittambulkar104@gmail.com>
2024-04-10 17:51:22 +0530

    chore: Fetch data from appwrite backend and render it on the screen

--- main.js
+++ main.js
@@ -1,10 +1,55 @@
 import "./style.css";
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 const client = new Client();
 
 client
   .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
-  .setProject(import.meta.env.VITE_APPWRITE_ACCOUNT);
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-console.log(client);
+const db = new Databases(client);
+const tasks = document.querySelector(".tasks");
+
+const getTasks = async function () {
+  try {
+    const response = await db.listDocuments(
+      import.meta.env.VITE_APPWRITE_DATABASE_ID,
+      import.meta.env.VITE_APPWRITE_COLLECTION_ID
+    );
+
+    const tasksList = response.documents;
+
+    console.log(tasksList);
+    tasksList.forEach((task) => {
+      let html = `
+      <div class="task" id="task-${task.$id}" data-id="${task.$id}">
+        <div class="task-select">
+          <div class="complete-${task.status} select-icon">&nbsp;</div>
+          <p class="task-text" id="task-${task.$id}">${task.body}</p>
+        </div>
+        <div class="update-delete">
+          <div id="update-icon">
+            <img
+              class="task-icon"
+              src="img/note-pencil-bold.svg"
+              alt="edit icon"
+            />
+          </div>
+          <div id="delete-icon">
+            <img
+              class="task-icon"
+              src="img/x-circle-bold.svg"
+              alt="close icon"
+            />
+          </div>
+        </div>
+    </div>`;
+
+      tasks.insertAdjacentHTML("beforeBegin", html);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+getTasks();
