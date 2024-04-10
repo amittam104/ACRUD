f94fd0df63fb70b993a25789ada1d7204c59a338
Amit Tambulkar <amittambulkar104@gmail.com>
2024-04-10 16:08:54 +0530

    chore: connected with appwrite backend

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import "./style.css";
+import { Client } from "appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint(import.meta.env.VITE_APPWRITE_URL)
+  .setProject(import.meta.env.VITE_APPWRITE_ACCOUNT);
+
+console.log(client);
